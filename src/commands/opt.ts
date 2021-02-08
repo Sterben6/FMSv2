1a3ba955751f62ecc9feaaaff456e49098443913
Sterben <oreozgamingofficial@gmail.com>
2021-02-07 19:36:15 -0600

    grammar fix for e&t
    Signed-off-by: Sterben <oreozgamingofficial@gmail.com>

--- src/commands/opt.ts
+++ src/commands/opt.ts
@@ -1,19 +1,44 @@
-import { Client, Command, RichEmbed, Collection } from '../class';
-import { Message, Member } from 'eris';
-
-export default class rank extends Command {
+import { Client, Command, RichEmbed } from '../class';
+import { Message } from 'eris';
+import opt_add from "./opt_add";
+export default class opt extends Command {
     constructor(client: Client) {
         super(client);
         this.name = 'opt';
         this.permissions = 0;
+        this.subcmds = [opt_add]
         this.enabled = true;
     }
 
     public async run(message: Message, args: string[]) {
-        if (args[0].toLowerCase() === "ssu") {
-            await message.member.addRole(`773026252285542430`)
-        } else if (args[0].toLowerCase() === "dev" || args[0].toLowerCase() === "dev notify") {
-            await message.member.addRole(`770421273113001995`)
+        if (!args[0]) {
+            const roles = await this.client.db.Role.find();
+            const embed = new RichEmbed();
+            embed.setTitle(`Roles`);
+            embed.setDescription(`Use \`-opt [role name]\` to be assigned/unassigned a role.`);
+            embed.setFooter(`Foundation Management Systems`);
+            embed.setTimestamp();
+
+            for (const role of roles) {
+                let roleInfo = ""
+                if (message.member.roles.includes(role.roleId)) roleInfo += `*You have this role*\n`;
+                if (role.description) roleInfo += `**Description:** ${role.description}\n`;
+                roleInfo += `**Permissions:** ${role.permission}\n`;
+                roleInfo += `**Mention:** <@&${role.roleId}>`;
+                embed.addField(role.key, roleInfo);
+            }
+
+            await message.channel.createMessage({ embed });
+            return;
         }
+    }
+
+    public PermToString = {
+        0: 'Everyone',
+        1: 'Level-3+',
+        2: 'Level-4+',
+        3: 'Site Director+',
+        4: 'Overseer Council+',
+        5: 'The Administrator'
     }
 }
